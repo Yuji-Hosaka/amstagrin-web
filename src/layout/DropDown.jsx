97538613c9e90a6c9617dca6db330138c68d2b3b
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-10-16 15:12:21 +0700

    config More button, remove switch account

--- src/layout/DropDown.jsx
+++ src/layout/DropDown.jsx
@@ -1,5 +1,5 @@
 import { CgDetailsMore } from "react-icons/cg";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
 import { useAuth } from "../hooks/use-auth";
 
@@ -25,21 +25,21 @@ export default function DropDown() {
 
   return (
     <div className=" relative" ref={dropDownEl}>
-      <div className=" cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
-        <CgDetailsMore />
+      <div className=" cursor-pointer flex items-center justify-center hover:bg-gray-100 p-2 gap-2 rounded-xl" onClick={() => setIsOpen(!isOpen)}>
+        <CgDetailsMore className="  shadow-xl" /> 
+      <div className=" font-semibold "> More</div>
       </div>
 
       {isOpen && (
-        <div className=" w-96 absolute bg-white right-0 translate-y-1 border rounded-xl shadow-xl p-2">
-          <Link to="/login">
-            <div className=" font-semibold"> Switch Accounts</div>
-          </Link>
+        <div className=" w-96 absolute bg-white right-0 translate-y-1 border rounded-xl shadow-xl p-2 ">
+          {/* <Link to="/login">
+            <div className=" font-semibold flex gap-4 p-2 items-center cursor-pointer hover:bg-gray-100 rounded-xl "> Switch Accounts</div>
+          </Link> */}
          
           <div
             className=" flex gap-4 p-2 items-center cursor-pointer hover:bg-gray-100 rounded-xl"
             onClick={logout}
           >
-            
             <div className=" font-semibold"> Log out</div>
           </div>
         </div>
